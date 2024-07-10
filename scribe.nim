a47c2ba419de69f0c7548b32b379f254d9bbf678
Saint <sainttttt@x265.ca>
2024-07-10 08:04:37 -0400

    tested both py and nim versions, got nim slightly faster added file checking to nim, nim is the main version now

--- scribe.nim
+++ scribe.nim
@@ -1,21 +1,48 @@
 import std/[strutils, streams, os, osproc,
-            strformat, json, times, paths, math]
+            strformat, times, paths, math]
 import print
 import xxhash
+import db_connector/db_sqlite
 
 
 let seed = 153.uint64
-let baseDir = "./"
-
 
 let scribeDir = (if paramCount() > 0 : paramStr(1) else: "./")
 
-let ignorePaths = @[".scribe", ".vim", ".git", "."]
+let dbPath = fmt"{scribeDir}/.scribe2.db"
+let db = open(dbPath, "", "", "")
 
-proc shouldIgnore(path: string, ignorePaths: seq[string]): bool =
+proc initDb(): void =
+  db.exec(sql"""CREATE TABLE IF NOT EXISTS hashes
+                      (name TEXT, filesize INTEGER, hash TEXT, date TEXT)""")
+
+  db.exec(sql"""CREATE TABLE IF NOT EXISTS files
+                      (name TEXT, filesize INTEGER, date TEXT)""")
+
+  db.exec(sql"""CREATE TABLE IF NOT EXISTS dates
+                      (date TEXT)""")
+
+  db.exec(sql"""CREATE INDEX IF NOT EXISTS hashes_name_idx
+                      ON hashes(name)""")
+
+  db.exec(sql"""CREATE INDEX IF NOT EXISTS files_name_idx
+                      ON files(name)""")
+
+  db.exec(sql"""CREATE INDEX IF NOT EXISTS files_date_idx
+                      ON files(date)""")
+
+  db.exec(sql"""CREATE INDEX IF NOT EXISTS files_name_date_idx
+                      ON files(name, date)""")
+
+
+
+proc shouldIgnore(path: string): bool =
+  let ignorePaths = @[".scribe", ".vim", ".git", "."]
   for pathPart in path.split('/'):
 
     for p in ignorePaths:
+      if p.len == 1: continue
+
       if pathPart.endsWith(p):
         return true
 
@@ -25,80 +52,107 @@ proc shouldIgnore(path: string, ignorePaths: seq[string]): bool =
   return false
 
 
+const chunkSize = 2 ^ 17
+var buffer = newSeqUninitialized[uint8](chunkSize)
+# var buffer = newString(chunkSize)
+
 proc streamingHash(path: string): string =
   var fs = newFileStream(path, fmRead)
   defer: fs.close()
+
   var state = newXxh3_64(seed)
-  const chunkSize = 2 ^ 16
-  var buffer: array[chunkSize, char]
   var size: int
 
-  try:
-    while not fs.atEnd:
-      size = fs.readData(addr(buffer), chunkSize)
-      state.update(cast[string](buffer[0..size-1]))
-  except:
-    print getCurrentExceptionMsg()
+  while not fs.atEnd:
+    # fs.readStr(chunkSize, buffer)
+    let size = fs.readData(addr(buffer[0]), chunkSize - 1)
+    buffer[size] = 0
 
-  return $(%*{path: state.digest().toHex.toLowerAscii})
+    state.update(cast[string](buffer))
 
+  return state.digest().toHex.toLowerAscii
+
+
+
+var insertCount = 0
+let COMMIT_RATE = 150
+
+var dateStr = now().format("yyyy-MM-dd' 'HH:mm:ss")
+var count = 0
+var txStarted = false
 proc hashFiles() =
+  for p in walkDirRec(scribeDir & "/"):
 
-  let mainScribeFolder = scribeDir & "/.scribe"
-  if not dirExists(mainScribeFolder):
-    print "doesn't exist"
-    discard execCmd(fmt"mkdir -p '{mainScribeFolder}'")
-
-
-  var dateStr = now().format("yyyy-MM-dd'_'HH-mm-ss")
-
-  let mainScribeFile = open(fmt"{mainScribeFolder}/{dateStr}.scribe",
-                     fmWrite)
-
-  defer: mainScribeFile.close()
-
-  var subRootScribeFile: File
-  defer: subRootScribeFile.close()
-
-  var prevSubroot: string
-  for path in walkDirRec(scribeDir & "/"):
-
-    var logPaths = @[scribeDir]
-    if path.shouldIgnore(ignorePaths):
+    if p.shouldIgnore:
       continue
 
-    let relpath =  relativePath(path, scribeDir)
-    let relPathSplit = relpath.splitPath
+    let path = p.normalizedPath
+    insertCount += 1
+    # if insertCount mod 1000 == 0:
+    #   print path
 
-    var subroot = ""
-    if not relPathSplit[0].isEmptyOrWhiteSpace:
-      subroot = relPathSplit[0].split("/")[0]
+    let filesize = path.getFileSize
 
-
-    # get hash
-
-    let hash = streamingHash(path)
-    print hash
-
-    mainScribeFile.writeLine(hash)
-
-    # write to main file
-
-    if true:
-      # wrap this in option to write to subroot
-      if subroot != "":
-        if subroot != prevSubroot:
-          prevSubroot = subroot
-          if subRootScribeFile != nil:
-            subRootScribeFile.close()
-          let subRootScribeDir = fmt"{scribeDir}/{subRoot}/.scribe/"
-          if not dirExists(subRootScribeDir):
-            discard execCmd(fmt"mkdir -p '{subRootScribeDir}'")
-          subRootScribeFile = open(fmt"{subRootScribeDir}/{dateStr}.scribe", fmWrite)
-        # write to subroot file
-        subRootScribeFile.writeLine(hash)
+    let fetchedSize = db.getValue(sql"""SELECT filesize FROM files
+                                      WHERE name = ? ORDER BY date
+                                      DESC LIMIT 1""", path)
 
 
-    # discard readline(stdin)
+    var hash = ""
+    if fetchedSize.len == 0 or fetchedSize.parseInt != filesize:
+      hash = streamingHash(path)
 
-hashFiles()
+    if not txStarted:
+      db.exec(sql"BEGIN")
+      txStarted = true
+
+    if hash == "":
+      db.exec(sql"INSERT INTO files(name, filesize, date) VALUES(?,?,?)",
+                 path,filesize, dateStr)
+    else:
+      db.exec(sql"INSERT INTO files(name, filesize, date) VALUES(?,?,?)",
+                 path,filesize, dateStr)
+
+      db.exec(sql"INSERT INTO hashes(name, filesize, date, hash) VALUES(?, ?,?,?)",
+                 path,filesize, dateStr, hash)
+    count += 1
+
+    if count mod 300 == 0:
+      db.exec(sql"COMMIT")
+      print "commit"
+      txStarted = false
+      count = 0
+
+    # print path
+    # print hash
+  if txStarted:
+    db.exec(sql"COMMIT")
+
+proc checkFiles() =
+  let lastTime = db.getValue(sql"""SELECT date FROM files
+                                    ORDER BY date
+                                    DESC LIMIT 1""")
+
+  for row in db.rows(sql"SELECT files.name, hashes.hash FROM files JOIN hashes ON files.name = hashes.name WHERE files.date = ?", lastTime):
+    if not row[0].fileExists:
+      print "not found"
+      print row[0], row[1]
+    else:
+      let newHash = streamingHash(row[0])
+      print newHash, row[1]
+      if newHash == row[1]:
+        print "match"
+      else:
+        print row[0]
+        print "no match ERROROROROROR"
+        discard readline stdin
+
+
+
+
+
+
+initDb()
+# hashFiles()
+# checkFiles()
+db.close()
--- scribe.py
+++ scribe.py
@@ -6,15 +6,19 @@ from datetime import datetime
 import sqlite3
 # import concurrent.futures
 
-db = sqlite3.connect(".scribe.db")
-cur = db.cursor()
 
 def init_db():
-    cur.execute("""CREATE TABLE IF NOT EXISTS files
+    cur.execute("""CREATE TABLE IF NOT EXISTS hashes
                         (name TEXT, filesize INTEGER, hash TEXT, date TEXT)""")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS files
+                        (name TEXT, filesize INTEGER, date TEXT)""")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS dates
                         (date TEXT)""")
+
+    cur.execute("""CREATE INDEX IF NOT EXISTS hashes_name_idx
+                        ON hashes(name)""")
 
     cur.execute("""CREATE INDEX IF NOT EXISTS files_name_idx
                         ON files(name)""")
@@ -25,18 +29,22 @@ def init_db():
     cur.execute("""CREATE INDEX IF NOT EXISTS files_name_date_idx
                         ON files(name, date)""")
 
-init_db()
 
 SEED=153
+COMMIT_RATE = 300
 
-scribe_dir = ".scribe"
 path = sys.argv[1]
 num_threads = 1
+
+db = sqlite3.connect(f"{path}/.scribe.db")
+cur = db.cursor()
+init_db()
+insert_count = 0
 
 # pool = concurrent.futures.ThreadPoolExecutor(max_workers=num_threads)
 
 def get_hash(file):
-    CHUNK_SIZE = 2 ** 32  # Or whatever you have memory to handle
+    CHUNK_SIZE = 2 ** 17  # Or whatever you have memory to handle
     with open(file, 'rb') as input_file:
         x = xxhash.xxh3_64(seed=SEED)
         for chunk in iter(partial(input_file.read, CHUNK_SIZE), b''):
@@ -44,25 +52,40 @@ def get_hash(file):
         return x.hexdigest()
 
 def write_hash(f, datetime, force_hash=False):
-    filesize = os.stat(f).st_size
+    global insert_count
+    try:
+        filesize = os.stat(f).st_size
 
-    cur.execute('SELECT name, filesize FROM files WHERE name = ? ORDER BY date DESC LIMIT 1', (f, ))
-    res = cur.fetchone()
+        cur.execute('SELECT name, filesize FROM files WHERE name = ? ORDER BY date DESC LIMIT 1', (f, ))
+        res = cur.fetchone()
 
-    h = ""
-    if not res:
-        h = get_hash(f)
-    elif res[1] != filesize:
-        h = get_hash(f)
 
-    if h == "":
-        d = (f,filesize, datetime,)
-        cur.execute("INSERT INTO files(name, filesize, date) VALUES(?,?,?)", (f,filesize, datetime))
-        print(f"{d=}")
-    else:
-        d = (f,filesize, datetime,h,)
-        cur.execute("INSERT INTO files(name, filesize, date, hash) VALUES(?,?,?,?)", (f,filesize, datetime, h))
-        print(f"{d=}")
+        h = ""
+        if not res:
+            h = get_hash(f)
+        elif res[1] != filesize:
+            h = get_hash(f)
+
+        d = None
+        if h == "":
+            d = (f,filesize, datetime,)
+            cur.execute("INSERT INTO files(name, filesize, date) VALUES(?,?,?)", (f,filesize, datetime))
+        else:
+            d = (f,filesize, datetime,h,)
+            cur.execute("INSERT INTO files(name, filesize, date) VALUES(?,?,?)", (f,filesize, datetime))
+            cur.execute("INSERT INTO hashes(name, filesize, date, hash) VALUES(?,?,?,?)", (f,filesize, datetime, h))
+
+        # print(f"{f=}")
+        # print(f"{h=}")
+
+        insert_count += 1
+        if not insert_count % COMMIT_RATE:
+            db.commit()
+            insert_count = 0
+
+
+    except FileNotFoundError as e:
+        print(e)
 
 
 
@@ -73,14 +96,16 @@ def get_datetime():
 
 def write_hashes():
     datetime = get_datetime()
+    count = 0
     for subdir, dirs, files in os.walk(path):
         for file in files:
             f = subdir + os.sep + file
             if os.path.isdir(f): continue
             if f.endswith(".scribe"): continue
 
-            print(f)
+            # print(f)
             write_hash(f, datetime)
+            count += 1
 
 
 write_hashes()
